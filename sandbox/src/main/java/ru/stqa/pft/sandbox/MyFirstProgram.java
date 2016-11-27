5d7234678be6fe90a419a1bf659e921406a57e29
mironuk-taisiya <mironuk.taisiya@gmail.com>
2016-11-27 13:58:01 +0200

    123

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -3,6 +3,10 @@ package ru.stqa.pft.sandbox;
 public class MyFirstProgram {
     public static void main(String[] args)
     {
-        System.out.println("Hi!");
+        String somebody="world";
+        System.out.println("Hello, "+somebody+"!");
+        double l=5.3;
+        double s=l*l;
+        System.out.println("Площадь квадрата со стороной "+l+"=" + s);
     }
 }
