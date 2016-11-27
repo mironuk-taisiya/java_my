e0f40cd6b0344b4b311935c046091a0eefa17462
mironuk-taisiya <mironuk.taisiya@gmail.com>
2016-11-27 14:23:32 +0200

    Class

---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Rectangle.java
@@ -0,0 +1,14 @@
+package ru.stqa.pft.sandbox;
+
+/**
+ * Created by Home on 27.11.2016.
+ */
+public class Rectangle {
+  public double a;
+  public double b;
+
+  public Rectangle(double a, double b) {
+    this.a=a;
+    this.b=b;
+  }
+}
---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Square.java
@@ -0,0 +1,13 @@
+package ru.stqa.pft.sandbox;
+
+/**
+ * Created by Home on 27.11.2016.
+ */
+public class Square {
+  public double l;
+  public Square(double l) {
+    this.l=l;
+  }
+}
+
+
