e376270c1e3dd1ee0e8b8217931f1ef5550420ff
mironuk-taisiya <mironuk.taisiya@gmail.com>
2016-11-28 22:54:52 +0200

    Home work

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -6,19 +6,23 @@ package ru.stqa.pft.sandbox;
 public class Point {
   Double x;
   Double y;
-  public static void main(String args[]){
-    Point p1=new Point();
-    p1.x=20.5;
-    p1.y=10.5;
-    System.out.println("Координаты первой точки:"+p1.x+" и "+p1.y);
-    Point p2=new Point();
-    p2.x=10.5;
-    p2.y=5.5;
-    System.out.println("Координаты второй точки:"+p2.x+" и "+p2.y);
-    System.out.println("Расстояние между токами="+distance(p1,p2));
+
+  public Point(Double x, Double y) {
+    this.x = x;
+    this.y = y;
   }
 
-  public static double distance(Point p1, Point p2){
-    return Math.sqrt(Math.pow(p1.x-p2.x,2)+Math.pow(p1.y-p2.y,2));
+  public static void main(String [] args){
+    Point p1=new Point(20.5,10.5);
+    System.out.println("Координаты первой точки:"+p1.x+" и "+p1.y);
+    Point p2=new Point(10.5,5.5);
+    System.out.println("Координаты второй точки:"+p2.x+" и "+p2.y);
+    System.out.println("Расстояние между точками="+distance(p1,p2));
+  }
+
+ public static double distance(Point p1, Point p2){
+    double d;
+    d=Math.sqrt(Math.pow(p1.x-p2.x,2)+Math.pow(p1.y-p2.y,2));
+    return d;
   }
 }
